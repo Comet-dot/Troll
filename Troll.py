6a91d0961baf35b0c0416ad0595ce741e08de8f7
Giovanni <82322898+ImAWildDogg@users.noreply.github.com>
2021-09-11 13:46:33 +0200

    Update Troll.py

--- Troll.py
+++ Troll.py
@@ -1,11 +1,11 @@
 from tkinter import *
 
-#parte principale
+#general
 window = Tk()
 window.title("Get trolled")
 window.configure(background="purple")
 
-#icona
+#image
 photo1 = PhotoImage(file="2.png")
 Label (window, image=photo1, bg="purple") .grid(row=0, column=0, sticky=E)
 
